2f45caab509f3048fe202d9260774f8c6b024238
Matias De la Puente <mfpuente.ar@gmail.com>
2022-09-30 16:38:37 -0300

    Clase del 2022-09-16 parte 2

--- 2022-09-16/02-objetos.py
+++ 2022-09-16/02-objetos.py
@@ -4,6 +4,22 @@
 #     nombre_completo: que devuelva apellido y nombre separado por coma
 #     es_mayor: que devuelva True si es mayor o igual a 18 años
  
+class Persona:
+  def __init__(self, nombre, apellido, edad):
+    self.nombre = nombre
+    self.apellido = apellido
+    self.edad = edad
+
+  def nombre_completo(self):
+    return self.apellido + ', ' + self.nombre
+  
+  def es_mayor(self):
+    return self.edad > 18
+
+persona = Persona('pepe', 'sanchez', 19)
+print('nombre completo:', persona.nombre_completo())
+print('es mayor:', persona.es_mayor())
+
 
 # Crear objeto Circulo:
 #   Propiedad: radio
